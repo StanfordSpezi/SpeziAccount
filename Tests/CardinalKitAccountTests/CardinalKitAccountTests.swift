5675c83fb8da989d275852e5b2d78a69f2da915b
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-04-03 15:50:09 -0700

    CardinalKit Account (#1)

---
+++ Tests/CardinalKitAccountTests/CardinalKitAccountTests.swift
@@ -0,0 +1,17 @@
+//
+// This source file is part of the CardinalKit open source project
+//
+// SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+@testable import CardinalKitAccount
+import XCTest
+
+
+final class CardinalKitAccountTests: XCTestCase {
+    func testCardinalKitAccount() throws {
+        XCTAssertTrue(true)
+    }
+}
