e6e0add0a8e74df0dd8764d912cb99e0c338f684
Andreas Bauer <andreas.bauer@stanford.edu>
2024-09-01 13:36:53 +0200

    accounRequired modifier handles anonymous accounts

--- Sources/SpeziAccount/ViewModifier/AccountRequiredModifier.swift
+++ Sources/SpeziAccount/ViewModifier/AccountRequiredModifier.swift
@@ -16,32 +16,39 @@ private let logger = Logger(subsystem: "edu.stanford.sepzi.SepziAccount", catego
 struct AccountRequiredModifier<SetupSheet: View>: ViewModifier {
     private let enabled: Bool
     private let setupSheet: SetupSheet
+    private let considerAnonymousAccounts: Bool
 
     @Environment(Account.self)
     private var account: Account? // make sure that the modifier can be used when account is not configured
 
     @State private var presentingSheet = false
 
+    private var shouldPresentSheet: Bool {
+        guard let account, enabled else {
+            return false
+        }
 
-    init(enabled: Bool, @ViewBuilder setupSheet: () -> SetupSheet) {
+        guard let details = account.details else {
+            return true // not signedIn
+        }
+
+        // we present the sheet if the account is anonymous and we do not consider anonymous accounts to the fully signed in
+        return details.isAnonymous && !considerAnonymousAccounts
+    }
+
+
+    init(enabled: Bool, considerAnonymousAccounts: Bool, @ViewBuilder setupSheet: () -> SetupSheet) {
         self.enabled = enabled
         self.setupSheet = setupSheet()
+        self.considerAnonymousAccounts = considerAnonymousAccounts
     }
 
 
     func body(content: Content) -> some View {
         content
-            .onChange(of: [account?.signedIn, presentingSheet]) {
-                guard enabled, let account else {
-                    return
-                }
-
-                if !account.signedIn {
-                    if !presentingSheet {
-                        presentingSheet = true
-                    }
-                } else {
-                    presentingSheet = false
+            .onChange(of: [shouldPresentSheet, presentingSheet]) {
+                if shouldPresentSheet != presentingSheet {
+                    presentingSheet = shouldPresentSheet
                 }
             }
             .task {
@@ -49,7 +56,7 @@ struct AccountRequiredModifier<SetupSheet: View>: ViewModifier {
                     return
                 }
 
-                guard let account else {
+                guard account != nil else {
                     logger.error("""
                                  accountRequired(_:setupSheet:) modifier was enabled but `Account` was not configured. \
                                  Make sure to include the `AccountConfiguration` the configuration section of your App delegate.
@@ -58,7 +65,7 @@ struct AccountRequiredModifier<SetupSheet: View>: ViewModifier {
                 }
 
                 try? await Task.sleep(for: .seconds(2))
-                if !account.signedIn {
+                if shouldPresentSheet {
                     presentingSheet = true
                 }
             }
@@ -81,10 +88,15 @@ extension View {
     ///
     /// - Parameters:
     ///   - required: The flag indicating if an account is required at all times.
+    ///   - considerAnonymousAccounts: Anonymous accounts are considered full accounts and fulfill the account requirements. See ``AccountDetails/isAnonymous``.
     ///   - setupSheet: The view that is presented if no account was detected. You may present the ``AccountSetup`` view here.
     ///     This view is directly used with the standard SwiftUI sheet modifier.
     /// - Returns: The modified view.
-    public func accountRequired<SetupSheet: View>(_ required: Bool = true, @ViewBuilder setupSheet: () -> SetupSheet) -> some View {
-        modifier(AccountRequiredModifier(enabled: required, setupSheet: setupSheet))
+    public func accountRequired<SetupSheet: View>(
+        _ required: Bool = true,
+        considerAnonymousAccounts: Bool = false,
+        @ViewBuilder setupSheet: () -> SetupSheet
+    ) -> some View {
+        modifier(AccountRequiredModifier(enabled: required, considerAnonymousAccounts: considerAnonymousAccounts, setupSheet: setupSheet))
     }
 }
