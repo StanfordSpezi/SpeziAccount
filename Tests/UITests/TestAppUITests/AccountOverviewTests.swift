dacd0c193a75006922c5efdc3363b6b11c1ded45
Andreas Bauer <andreas.bauer@stanford.edu>
2024-10-30 15:42:57 +0100

    Fix compilation

--- Tests/UITests/TestAppUITests/AccountOverviewTests.swift
+++ Tests/UITests/TestAppUITests/AccountOverviewTests.swift
@@ -63,7 +63,7 @@ final class AccountOverviewTests: XCTestCase { // swiftlint:disable:this type_bo
         app.updateGenderIdentity(from: "Male", to: "Choose not to answer")
 #if !os(visionOS)
         // on visionOS we are currently unable to tap on date pickers :)
-        app.changeDatePreviousMonthFirstDay()
+        app.changeDateOfBirth()
 #endif
 
         XCTAssertTrue(app.buttons["Add Biography"].exists)
