eecf81701481cba9550a5dc86f43caabd4bbd1c3
Andreas Bauer <andreas.bauer@stanford.edu>
2024-12-15 22:58:15 +0100

    Move SignupFormHeader to SpeziViews as ListHeader

--- Sources/SpeziAccount/Views/SignupFormHeader.swift
+++ Sources/SpeziAccount/Views/SignupFormHeader.swift
@@ -6,50 +6,13 @@
 // SPDX-License-Identifier: MIT
 //
 
+import SpeziViews
 import SwiftUI
-
-
-struct FormHeader<Image: View, Title: View, Instructions: View>: View {
-    private let image: Image
-    private let title: Title
-    private let instructions: Instructions
-
-
-    var body: some View {
-        VStack {
-            VStack {
-                image
-                    .foregroundColor(.accentColor)
-                    .symbolRenderingMode(.multicolor)
-                    .font(.custom("XXL", size: 50, relativeTo: .title))
-                    .accessibilityHidden(true)
-                title
-                    .accessibilityAddTraits(.isHeader)
-                    .font(.title)
-                    .bold()
-                    .padding(.bottom, 4)
-            }
-                .accessibilityElement(children: .combine)
-            instructions
-                .padding([.leading, .trailing], 25)
-        }
-            .multilineTextAlignment(.center)
-            .frame(maxWidth: .infinity)
-    }
-
-    init(@ViewBuilder image: () -> Image, @ViewBuilder title: () -> Title, @ViewBuilder instructions: () -> Instructions) {
-        self.image = image()
-        self.title = title()
-        self.instructions = instructions()
-    }
-}
 
 
 public struct SignupFormHeader: View {
     public var body: some View {
-        FormHeader {
-            Image(systemName: "person.fill.badge.plus") // swiftlint:disable:this accessibility_label_for_image
-        } title: {
+        ListHeader(systemImage: "person.fill.badge.plus") {
             Text("UP_SIGNUP_HEADER", bundle: .module)
         } instructions: {
             Text("UP_SIGNUP_INSTRUCTIONS", bundle: .module)
