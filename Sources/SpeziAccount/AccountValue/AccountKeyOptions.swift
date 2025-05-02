f7cf2b17b14f6458c2e56184e44b646afa342aae
Andreas Bauer <andreas.bauer@stanford.edu>
2025-05-02 15:05:47 +0200

    Introduce display and mutable options instead

--- Sources/SpeziAccount/AccountValue/AccountKeyOptions.swift
+++ Sources/SpeziAccount/AccountValue/AccountKeyOptions.swift
@@ -7,7 +7,8 @@
 //
 
 
-public struct AccountKeyOptions: OptionSet { // TODO: docs!
+/// Options associated with an Account Key.
+public struct AccountKeyOptions: OptionSet {
     public let rawValue: UInt64
 
     public init(rawValue: UInt64) {
@@ -20,8 +21,23 @@ extension AccountKeyOptions: Hashable, Sendable, Codable {}
 
 
 extension AccountKeyOptions {
-    public static let read = AccountKeyOptions(rawValue: 1 << 0)
-    public static let write = AccountKeyOptions(rawValue: 1 << 1)
+    /// The account key supports to be displayed to the user.
+    ///
+    /// An Account Key always implicitly supports read access. Therefore, when `display` is supported, a ``DataDisplayView`` is required to be specified (or relying on a default implementation fo
+    /// the given type).
+    /// If ``mutable`` option is set as well, a ``DataEntryView`` is likewise required to be specified.
+    public static let display = AccountKeyOptions(rawValue: 1 << 0)
+    /// The account key supports mutation.
+    ///
+    /// This option marks the account key to support user-initiated (or think of as client-side initiated) mutation.
+    /// Having this option not set does not prevent server-side permutation (e.g., as a side effect of some request).
+    /// Instead it makes it explicit that the user (or rather client-side code) is allowed to mutate the value themselves (assuming some authorization checks).
+    ///
+    /// If the the ``display`` option is set, you must supply a ``DataEntryView`` to allow permutation from UI.
+    public static let mutable = AccountKeyOptions(rawValue: 1 << 1)
 
-    public static let `default`: AccountKeyOptions = [.read, .write]
+    /// The default configuration for an Account Key.
+    ///
+    /// By default, an Account Key is ``display``ed and ``mutable``.
+    public static let `default`: AccountKeyOptions = [.display, .mutable]
 }
