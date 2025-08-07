e56fbceb14d1de79afc8c01f52ad7c8aecc68203
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2025-08-07 09:27:36 -0700

    Update Docs

--- Sources/SpeziAccount/AccountValue/AccountKey.swift
+++ Sources/SpeziAccount/AccountValue/AccountKey.swift
@@ -12,6 +12,7 @@ import SwiftUI
 
 
 #if compiler(<6.2)
+/// Pre Swift 6.2 typealias for `SendableMetatype` that resolves as `Any` to avoid larger `#if` statements in the code base.
 public typealias SendableMetatype = Any
 #endif
 
